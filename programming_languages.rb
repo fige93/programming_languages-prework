f8d57b8eae53e37792d4f860efa82c5c7e0ec61d
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-28 08:55:51 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |name, attributes|
-    new_hash[name] = attributes
-    binding.pry
     if name == :javascript && style == :functional
     else
+      new_hash[name] = attributes
       new_hash[name][:style] = []
     end
     new_hash[name][:style] << style
